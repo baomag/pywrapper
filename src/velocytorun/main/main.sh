2d6c2d466d7187402bab5d6535c9de92c63b5de2
Yourong Bao <maggiemaizibao@gmail.com>
2023-08-23 14:14:07 -0500

    notes added for wrappers, added meta.csv

--- src/velocytorun/main/main.sh
+++ src/velocytorun/main/main.sh
@@ -6,4 +6,5 @@ barcode=/storage/singlecell/jeanl/organoid/data/D45_organoid_Sridhar/D45_organoi
 bam=/storage/singlecell/jeanl/organoid/data/D45_organoid_Sridhar/D45_organoid/outs/possorted_genome_bam.bam
 genome=/storage/singlecell/zz4/Reference/refdata-cellranger-arc-GRCh38-2020-A/genes/genes.gtf
 
-slurmtaco.sh -n g01 -- python ../velocytorun.py -e velocyto -d "$outdir" -b test -c "$barcode" -g "$genome" -t 8 -- "$bam"
+export TMPDIR=$outdir
+slurmtaco.sh -n g01 -- python ../velocytorun.py -e velocyto -d "$outdir" -b "$bname" -c "$barcode" -g "$genome" -t 8 -- "$bam"
--- src/velocytorun/velocytorun.py
+++ src/velocytorun/velocytorun.py
@@ -9,7 +9,7 @@ import click
 
 CONTEXT_SETTINGS=dict(help_option_names=['-h', '--help'])
 @click.command(context_settings=CONTEXT_SETTINGS)
-@click.option('-d', '--outdir', type=click.Path(), default='.', show_default=True, help='Outdir.')
+@click.option('-d', '--outdir', type=click.Path(resolve_path=True), default='.', show_default=True, help='Outdir.')
 @click.option('-b', '--bname', type=click.STRING, required=True, help='Bname.')
 @click.option('-e', '--condaenv', type=click.STRING, help='Conda environment.')
 @click.option('-c', '--barcode', type=click.Path(exists=True), required=True, help='Valid barcode.')
@@ -21,14 +21,20 @@ def main(outdir, bname, condaenv, barcode, gtf, numthreads, infile):
 Run velocyto function to calculate RNA velocity.
 
 INFILE is the BAM file.
+OUTFILE is LOOM file.
 
 \b
 Example:
-  velocytorun -d "$outdir" -b "$bname" -- xx.bam
+	bname=D45_organoid_Sridhar
+	barcode=/storage/singlecell/jeanl/organoid/data/D45_organoid_Sridhar/D45_organoid/outs/filtered_feature_bc_matrix/barcodes.tsv.gz
+	ban=/storage/singlecell/jeanl/organoid/data/D45_organoid_Sridhar/D45_organoid/outs/possorted_genome_bam.bam
+  velocytorun -d "$outdir" -b "$bname" -e velocyto -c "$barcode" -g /storage/singlecell/zz4/Reference/refdata-cellranger-arc-GRCh38-2020-A/genes/genes.gtf -t 8 -- xx.bam
 
 \b
 Note:
-  1. 
+	Samtools may produce temporary files and requires writing access of input directory
+	To solve this, create a temporary copy of the input files to the working directory and delete it at the end of the code
+	Or, create soft link from the working directory to the originial input files (but may not bypass the access restrictions)
 
 \b
 See also:
@@ -36,21 +42,24 @@ See also:
     Velocyto
 
 \b
-Date: 2023/08/17
+Date: 2023/08/23
 Authors: Yourong Bao <maggiemaizibao@gmail.com>
 """
-	Path(outdir).mkdir(parents=True, exist_ok=True)
-	absdir=Path(__file__).parent
-	scriptname=Path(__file__).stem
+	Path(f"{outdir}/{bname}").mkdir(parents=True, exist_ok=True)
+	inbname=Path(infile).name
+	tmpfile=Path(outdir) / bname / inbname
+	# lnfile.hardlink_to(infile)
 	# os.chdir(outdir)
 	exprs=[
-		# f"ln -s {infile} {outdir};",
+		f"cp {infile} {tmpfile};",
+		f"cp {infile}.bai {tmpfile}.bai;",
 		f"velocyto run",
 		f"-o '{outdir}/{bname}'",
 		f"-b '{barcode}'",
 		f"-@ {numthreads}",
-		f"'{infile}'",
-		f"'{gtf}'",
+		f"'{tmpfile}'",
+		f"'{gtf}';",
+		f"rm -f {tmpfile} {tmpfile}.bai;",
 		]
 	return exeBash.callback(exprs, condaenv=condaenv, verbose=True)
 
