6db71c6e787b8002087a4a487d9b04c5b0e470ed
Yourong Bao <maggiemaizibao@gmail.com>
2023-08-23 15:37:46 -0500

    subtle changes in format

--- src/velocytorun/main/main.sh
+++ src/velocytorun/main/main.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# echo all commands
 set -x
 outdir=$(mrrdir.sh)
 barcode=/storage/singlecell/jeanl/organoid/data/D45_organoid_Sridhar/D45_organoid/outs/filtered_feature_bc_matrix/barcodes.tsv.gz
@@ -7,4 +8,4 @@ bam=/storage/singlecell/jeanl/organoid/data/D45_organoid_Sridhar/D45_organoid/ou
 genome=/storage/singlecell/zz4/Reference/refdata-cellranger-arc-GRCh38-2020-A/genes/genes.gtf
 
 export TMPDIR=$outdir
-slurmtaco.sh -n g01 -- python ../velocytorun.py -e velocyto -d "$outdir" -b "$bname" -c "$barcode" -g "$genome" -t 8 -- "$bam"
+slurmtaco.sh -n d03 -- python ../velocytorun.py -e velocyto -d "$outdir" -b test -c "$barcode" -g "$genome" -t 8 -- "$bam"
